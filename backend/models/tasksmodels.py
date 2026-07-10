4d9de78d6f52553d6d1efe3a1613cfa94abb340e
Thaneluna <42688917+Thaneluna@users.noreply.github.com>
2026-07-09 22:58:46 -1000

    Setup backend fastapi database

---
+++ backend/models/tasksmodels.py
@@ -0,0 +1,15 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Enum, Date
+from backend.routes.storage import engine as storage_engine
+
+Base = declarative_base()
+
+class Tasks(Base):
+    __tablename__ = "tasks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    date = Column(Date, nullable=False)
+    dateindex = Column(Integer, nullable=False)
+    
+Base.metadata.create_all(bind=storage_engine)
---
+++ backend/routes/storage.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine, Column, Integer, String, Enum
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
+
+from pydantic import BaseModel
+from typing import Optional, List
+
+engine = create_engine("sqlite:///./issues.db", connect_args={"check_same_thread": False})
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
