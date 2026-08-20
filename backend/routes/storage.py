7a521d811cefae41d28edda97ca22a6f5c2460dc
Thaneluna <42688917+Thaneluna@users.noreply.github.com>
2026-08-20 07:16:57 -1000

    Updated database

--- backend/routes/storage.py
+++ backend/routes/storage.py
@@ -1,17 +1,20 @@
-from sqlalchemy import create_engine, Column, Integer, String, Enum
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, Session
-
-from pydantic import BaseModel
-from typing import Optional, List
-
 import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = os.environ["DATABASE_URL"]
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine,
+)
 
 def get_db():
     db = SessionLocal()
